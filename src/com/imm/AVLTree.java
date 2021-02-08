e1766dd0ecbbca1b8e84dfc3f81f05c501cb8199
iranga <irangacomz@gmail.com>
2021-02-08 21:06:09 +0530

    fixup! Implementation for insert function to AVL Trees

--- src/com/imm/AVLTree.java
+++ src/com/imm/AVLTree.java
@@ -1,12 +1,12 @@
 package com.imm;
 
 public class AVLTree {
-    private class Node{
+    private class AVLNode{
         private int value;
-        private int leftChild;
-        private int rightChild;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
 
-        public Node(int value) {
+        public AVLNode(int value) {
             this.value = value;
         }
 
@@ -16,7 +16,33 @@ public class AVLTree {
         }
     }
 
-    private Node root;
+    private AVLNode root;
 
+    public void insert(int value){
+        root = insert(root,value);
+    }
+    private AVLNode insert(AVLNode root,int value){
+        //      10(rot)
+        //    /         \
+        //  Null(LC)    Null(RC)
 
+        //      10(rot)
+        //    /         \
+        //  4(LC)    Null(RC)
+
+        //      10(rot)
+        //    /         \
+        //  4(LC)    20(RC)
+
+        //base condition
+        if(root == null)
+            return new AVLNode(value);
+
+        if(value < root.value) //go to left side recursively
+            root.leftChild = insert(root.leftChild,value);
+        else //go to right side recursively
+            root.rightChild = insert(root.rightChild,value);
+        //System.out.println("inseerting root");
+        return root;
+    }
 }
