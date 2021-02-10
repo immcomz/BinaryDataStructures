7013fe2b9a6be6ba0b1fff09c426a9f113325588
iranga <irangacomz@gmail.com>
2021-02-10 19:12:15 +0530

    Initial Commit for Heap

---
+++ src/com/imm/Heap.java
@@ -0,0 +1,15 @@
+package com.imm;
+
+public class Heap {
+  // Charadteristics childs < parent | values filled from lef to right
+ //  Balanced  20 (max value)        20 (max value) Not Balanced
+ //        5       15             5       15
+ //      4   5    6            4     5   ?    6
+ //
+ //
+
+    private int[] items = new int[10];
+    private int size; //to keep track of number of items in an array/heap
+
+
+}
