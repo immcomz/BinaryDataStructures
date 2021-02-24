e66de6e201071efd5bd9ebd156605aa3bdb3333d
iranga <irangacomz@gmail.com>
2021-02-24 18:03:56 +0530

    Initial Commit for Heap and Implementstion for Insert Method

--- src/com/imm/Heaps.java
+++ src/com/imm/Heaps.java
@@ -1,12 +1,12 @@
 package com.imm;
 
-public class Heap {
-  // Charadteristics childs < parent | values filled from lef to right
- //  Balanced  20 (max value)        20 (max value) Not Balanced
- //        5       15             5       15
- //      4   5    6            4     5   ?    6
- //if parent < child swap the indexes/ bubble up indexes
- //
+public class Heaps {
+    // Charadteristics childs < parent | values filled from lef to right
+    //  Balanced  20 (max value)        20 (max value) Not Balanced
+    //        5       15             5       15
+    //      4   5    6            4     5   ?    6
+    //if parent < child swap the indexes/ bubble up indexes
+    //
 
     private int[] items = new int[10];
     private int size; //to keep track of number of items in an array/heap
