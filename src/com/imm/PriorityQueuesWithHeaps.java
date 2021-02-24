4e392e4717c338fdd624c7a5cf34d22f4ed25c45
iranga <irangacomz@gmail.com>
2021-02-24 22:02:14 +0530

    Implementation to Priority Queues Using Heaps

--- src/com/imm/PriorityQueuesWithHeaps.java
+++ src/com/imm/PriorityQueuesWithHeaps.java
@@ -4,9 +4,11 @@ public class PriorityQueuesWithHeaps {
     private Heaps heap = new Heaps();
 
     public void enqueue(int item){
+        //O(log n)
         heap.insert(item);
     }
     public int dequeue(){
+        //O(log n)
         return heap.remove();
     }
     public boolean isEmpty(){
