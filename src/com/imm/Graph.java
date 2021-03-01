5ee7bd61e5532aeee29a5e2c1f5437dfc93cd9d7
iranga <irangacomz@gmail.com>
2021-03-01 21:44:26 +0530

    Implementation Breadth-first Traversal (Iterative) in a Graph

--- src/com/imm/Graph.java
+++ src/com/imm/Graph.java
@@ -114,7 +114,6 @@ public class Graph {
 
         }
     }
-
     public void traverseDepthFirstIterate(String root){
         var node = nodes.get(root);
         if(node == null) return;
@@ -142,6 +141,28 @@ public class Graph {
             }
         }
     }
+    public void traverseBreadthFirst(String root) {
+        var node = nodes.get(root);
+        if (node == null)
+            return;
 
+        Set<Node> visited = new HashSet<>();
 
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+
+            if (visited.contains(current))
+                continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbour : adjacencyList.get(current))
+                if (!visited.contains(neighbour))
+                    queue.add(neighbour);
+        }
+    }
 }
